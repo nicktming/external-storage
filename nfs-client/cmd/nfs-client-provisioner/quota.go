cfba82747bc64263447b93027bfc8820a07282b5
nicktming <2279563278@qq.com>
2020-10-25 17:24:02 +0800

    add quota

--- nfs-client/cmd/nfs-client-provisioner/quota.go
+++ nfs-client/cmd/nfs-client-provisioner/quota.go
@@ -172,6 +172,10 @@ func (q *xfsQuotaer) AddProject(directory, bhard string) (string, uint16, error)
 	// Specify the new project
 	cmd := exec.Command("xfs_quota", "-x", "-c", fmt.Sprintf("project -s -p %s %s", directory, projectIDStr), q.xfsPath)
 	out, err := cmd.CombinedOutput()
+
+	fmt.Printf("exec %s, out: %v with error: %v\n",
+		fmt.Sprintf("project -s -p %s %s", directory, projectIDStr), out, err)
+
 	if err != nil {
 		deleteID(q.mapMutex, q.projectIDs, projectID)
 		removeFromFile(q.fileMutex, q.projectsFile, block)
